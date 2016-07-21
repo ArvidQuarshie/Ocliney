5de17acbfffb4a597cafe0740e5bed281493203b
Kathure <lizkathure@gmail.com>
2016-07-21 17:11:11 +0300

    added dependencies

---
+++ app/src/main/java/com/example/user/ocliney/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.ocliney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
