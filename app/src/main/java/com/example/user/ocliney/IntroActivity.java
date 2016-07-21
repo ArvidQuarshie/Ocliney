f05d33b60e31afee777040c72dfb87dc7191863c
Kathure <lizkathure@gmail.com>
2016-07-21 17:31:48 +0300

    added sampleslide.xml

--- app/src/main/java/com/example/user/ocliney/IntroActivity.java
+++ app/src/main/java/com/example/user/ocliney/IntroActivity.java
@@ -1,5 +1,8 @@
 package com.example.user.ocliney;
 
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +13,41 @@ public class IntroActivity extends AppIntro {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        addSlide(SampleSlide.newInstance(R.layout.activity_intro));
+        addSlide(SampleSlide.newInstance(R.layout.your_slide_here));
+        addSlide(SampleSlide.newInstance(R.layout.your_slide_here));
+        addSlide(SampleSlide.newInstance(R.layout.your_slide_here));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(true);
+        setProgressButtonEnabled(true);
+
+
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
 
 
     }
